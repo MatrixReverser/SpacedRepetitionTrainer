6ea3806afdfe402c1d4165c40d8376ac95137797
Andreas Altwein <kontakt@andreas-altwein.de>
2024-07-21 05:56:32 +0200

    used .gitignore file

---
+++ SpacedRepetitionTrainer/Vocabulary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpacedRepetitionTrainer
+{
+    class Word
+    {
+        public string Term { get; set; }
+        public string[] Translation { get; set; }
+        public long Timestamp { get; set; }
+        public int Level { get; set; }
+
+        public Word() 
+        {
+            Term = string.Empty;
+            Translation = new string[] { String.Empty, String.Empty, String.Empty };
+            Timestamp = 0;
+            Level = 0;
+        }
+    }
+
+    class VocabularySet
+    {
+        private List<Word> words;
+
+        public VocabularySet()
+        {
+            words = new List<Word>();
+        }
+    }
+}
