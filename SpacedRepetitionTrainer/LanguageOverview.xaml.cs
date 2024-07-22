585f03aa248e39c5fe00e4631673847e0fac8af3
Andreas Altwein <kontakt@andreas-altwein.de>
2024-07-22 21:07:43 +0200

    added table for the vocabularies

--- SpacedRepetitionTrainer/LanguageOverview.xaml.cs
+++ SpacedRepetitionTrainer/LanguageOverview.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,12 +33,63 @@ namespace SpacedRepetitionTrainer
             _vocabularySet.Load();
 
             InitInfoBlock();
+            InitDataGrid();
         }
 
         private void InitInfoBlock()
         {
             Description.Text = _vocabularySet.Description;
             WordCount.Text = "Wörter: " + _vocabularySet.GetWordCount();
+        }
+
+        private void InitDataGrid()
+        {
+            VocabularyGrid.ItemsSource = _vocabularySet.Words;
+        }
+    }
+
+    public class StringArrayToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string[] array)
+            {
+                return string.Join(", ", array);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return str.Split(new[] { ", " }, StringSplitOptions.None);
+            }
+            return new string[0];
+        }
+    }
+
+    public class UnixTimestampToDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is long unixTimestamp)
+            {
+                // Unix timestamp is seconds past epoch
+                DateTime dateTime = DateTimeOffset.FromUnixTimeSeconds(unixTimestamp).DateTime;
+                // Return only the date part in German format
+                return dateTime.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str && DateTime.TryParseExact(str, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime))
+            {
+                return new DateTimeOffset(dateTime).ToUnixTimeSeconds();
+            }
+            return 0L;
         }
     }
 }
--- SpacedRepetitionTrainer/Vocabulary.cs
+++ SpacedRepetitionTrainer/Vocabulary.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace SpacedRepetitionTrainer
 {
